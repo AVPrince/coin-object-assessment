fc67ce0694717b54cf727446760e678bdf487708
avprince <shimmyaries@ALAN-TRACYs-MacBook-Air.local>
2020-11-17 13:28:50 -0600

    updated methods

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Let's Roll!");
+console.log("Coin Flip!");
 
 let coin = {
     state: 0,
@@ -7,9 +7,10 @@ let coin = {
         this.state = Math.floor(Math.random() * 2);
         /* 1. Randomly set your coin object's "state" property to be either 
            0 or 1: use "this.state" to access the "state" property on this object.*/
-           return this.state
+        return this.state
     },
     toString: function() {
+        this.flip()
         if(this.state === 0){
             return "Heads"
         } else if(this.state === 1){
@@ -22,28 +23,28 @@ let coin = {
         let image = document.createElement('img');
         let heads = "./images/heads.jpg"
         let tails = "./images/tails.jpg"
+        this.flip()
         if (this.state === 0) {
             image.src = heads;
          } else if(this.state === 1){
                 image.src = tails;
         }
+        image.style.height = "100px"
+        image.style.width = "100px"
         return image;
         /* 3. Set the properties of this image element to show either face-up
            or face-down, depending on whether this.state is 0 or 1.*/
     }
 };
-
-console.log(coin.toString(coin.flip()));
+console.log(coin.toString());
 
 let container = document.createElement('div');
 container.append(coin.toString())
-container.style.width = '100px';
-container.style.height = '100px';
 document.body.append(coin.toHTML())
 
 function display20Flips (){
     for (let i = 0; i < 20; i++) {
-        document.body.append(coin.toString())
+        container.append(coin.toString())
     }
     /* Write a function called display20Flips that uses a loop to flip the coin 20 times. Each time the coin flips, display the result of each flip as a string on the page (make use of your toString() method) */
 }
@@ -55,6 +56,7 @@ function display20Images() {
     /* Write a function called display20Images, again using a loop to flip the coin 20 times…but this time instead of displaying the result as a string, display the result of each flip as an HTML IMG element on the page (make use of your toHTML() method).create them dynamically with Javascript and append them to your HTML using document.createElement and ParentNode.append */
 }
 
+display20Images();
 
 // let fakeDiv = document.createElement('div');
 // fakeDiv.style.backgroundColor = "blue";
